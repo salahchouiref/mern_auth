84fc26b78c1b79b8c9d76070906cd6b0a450a334
salahchouiref <popomaha7@gmail.com>
2023-10-26 18:10:22 +0100

    create and run the nodejs and express server

---
+++ api/index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/",(req,res)=>{
+    res.send("hello world");
+});
+
+app.listen(3000,()=>{
+    console.log("app is running on http://localhost:3000")
+})
