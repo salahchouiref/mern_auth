459139dd7fa58fb6aaca4a6912d5a797dbf66a30
salahchouiref <popomaha7@gmail.com>
2023-10-27 13:39:43 +0100

    create signup api and test it

--- api/index.js
+++ api/index.js
@@ -2,7 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
+const app = express();
+
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to database using mongoose");
@@ -10,10 +14,10 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log(err);
 })
 
-const app = express();
 
-app.use("/api/user",userRoutes);
-
-app.listen(3000,()=>{
+app.listen(3000 || process.env.PORT,()=>{
     console.log("app is running on http://localhost:3000")
 })
+
+app.use("/api/user",userRoutes);
+app.use("/api/auth",authRoutes);
