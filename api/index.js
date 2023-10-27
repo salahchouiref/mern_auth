3b283f8bbd6deab8e9f7ded569a33b4fffd9edf5
salahchouiref <popomaha7@gmail.com>
2023-10-27 14:32:02 +0100

    add error middleware & customized error method

--- api/index.js
+++ api/index.js
@@ -12,12 +12,22 @@ mongoose.connect(process.env.MONGO).then(()=>{
     console.log("connected to database using mongoose");
 }).catch((err)=>{
     console.log(err);
-})
+});
 
 
 app.listen(3000 || process.env.PORT,()=>{
     console.log("app is running on http://localhost:3000")
-})
+});
 
 app.use("/api/user",userRoutes);
 app.use("/api/auth",authRoutes);
+
+app.use((err,req,res,next)=>{
+    const statusCode = err.status || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success : false,
+        message,
+        statusCode,
+    });
+});
